27fb97539307086db6e62ed5f4ca7a10361bc96d
Bruno Perry <brunoperry@Brunos-MBP.lan>
2022-07-10 15:21:14 +0100

    update

--- bin/www.js
+++ bin/www.js
@@ -5,12 +5,12 @@
 /**
  * Module dependencies.
  */
-import app from '../app.js';
+import app from "../app.js";
 
-import http from 'http';
+import http from "http";
 
-import LogRocket from 'logrocket';
-LogRocket.init('a7h2qk/napontadalingua');
+// import LogRocket from 'logrocket';
+// LogRocket.init('a7h2qk/napontadalingua');
 
 /**
  * Normalize a port into a number, string, or false.
@@ -20,51 +20,46 @@ const normalizePort = (val) => {
   if (isNaN(port)) return val;
   if (port >= 0) return port;
   return false;
-}
+};
 
 /**
  * Event listener for HTTP server "error" event.
  */
 const onError = (error) => {
-  if (error.syscall !== 'listen') throw error;
+  if (error.syscall !== "listen") throw error;
 
-  let bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  let bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
       throw error;
   }
-}
+};
 /**
  * Event listener for HTTP server "listening" event.
  */
 const onListening = () => {
   const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
 
-  console.log('Na Ponta da Lingua listening on', bind);
-}
-
+  console.log("Na Ponta da Lingua listening on", bind);
+};
 
 /**
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
 /**
  * Create HTTP server.
@@ -75,7 +70,5 @@ const server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-
+server.on("error", onError);
+server.on("listening", onListening);
